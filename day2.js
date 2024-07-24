87d2a0f280df1b700e6b0ac8916b5c7f945649bb
510kirti <kirtigoyal5104@gmail.com>
2024-07-24 13:08:25 +0530

    addeed day2.js

---
+++ day2.js
@@ -0,0 +1,93 @@
+//OPERATORS
+
+//Activity - 1
+
+//Task - 1
+let a = 2;
+let b = 4;
+console.log(a+b);
+
+//Task - 2
+let x = 5;
+let y = 1;
+console.log(x-y);
+
+//Task - 3
+let m = 4;
+let n = 6;
+console.log(m*n);
+
+//Task - 4
+let var1 = 10;
+let var2 = 5;
+console.log(var1/var2);
+
+//Task - 5
+let i = 9;
+let j = 4;
+console.log(i%j);
+
+
+//Activity - 2
+
+//Task - 6
+let var3 = 5;
+var3 += 2;
+console.log(var3);
+
+//Task - 7
+let var4 = 5;
+var4 -= 2;
+console.log(var4);
+
+
+//Activity - 3
+
+//Task - 8
+let var5 = 9;
+let var6 = 8;
+console.log(var5 > var6);
+console.log(var6 > var5);
+
+
+//Task - 9
+let var7 = 10;
+let var8 = 13;
+console.log(var7 >= var8);
+console.log(var7 <= var8);
+
+
+//Task - 10
+let c = 1;
+let d = true;
+console.log(c == d);
+console.log(c === d);
+
+
+//Activity - 4
+
+//Task - 11
+let w = 5;
+let z = 7;
+if(w==5 && z>w)
+    console.log("yes");
+else console.log("no");
+
+
+//Task - 12
+let num1 = 9;
+let num2 = 7;
+if(num1 > num2 || num1 == num2)
+    console.log("yes");
+else console.log("no");
+
+//Task - 13
+let num3 = true;
+console.log(!num3);
+
+
+//Activity - 5
+
+//Task - 14
+let num4 = -6;
+num4>0?console.log("num4 is positive"):console.log("num4 is negative");
