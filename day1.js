c960bb692f044d1a2a6922e447859ccce332d6a8
510kirti <kirtigoyal5104@gmail.com>
2024-07-24 12:32:56 +0530

    added code to day1.js

--- day1.js
+++ day1.js
@@ -1,2 +1,49 @@
+//Activity -1
+
+//Task - 1
 var a = 5;
 console.log(a);
+
+//Task-2
+let b = "hello";
+console.log(b);
+
+//Activity-2
+
+//Task -3
+const c = true;
+console.log(c);
+
+//Activity-3
+
+//Task-4
+let var1 = 2;
+let var2 = "world";
+let var3 = false;
+let var4 = {
+  firstName: "Kirti",
+  latName: "Goyal",
+};
+let var5 = [1, 2, 3, 4, 5];
+
+console.log(typeof var1);
+console.log(typeof var2);
+console.log(typeof var3);
+console.log(typeof var4);
+console.log(typeof var5);
+
+//Activity -4
+
+//Task -5
+let x = 9;
+console.log(x);
+x = 10;
+console.log(x);
+
+//Activity -5
+
+//Task -6
+const y = 5;
+console.log(y);
+y = 10;
+console.log(y);
